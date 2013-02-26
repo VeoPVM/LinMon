2b942e28f0cec5c58742208d62bdd49aec712024
Perri Vardy-Mason <perri@veoserv.com>
2013-02-26 22:59:26 +0000

    Add master daemon.  Currently very limited.

---
+++ master/index.php
@@ -0,0 +1,33 @@
+<?php
+set_time_limit(0);
+
+include 'config/config.php';
+
+// Create socket
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+
+// Bind to socket
+socket_bind($socket, $config, 6969) or die('Could not bind to address');
+
+// Listen to socket
+socket_listen($socket);
+
+while (true){
+$client = socket_accept($socket);
+
+
+// Read client input
+//$input = socket_read($client, 2048);
+
+$output = '
+Connected to LinMon!
+';
+
+// Send output to client
+socket_write($client, $output);
+
+socket_close($client);
+usleep(10000);
+}
+socket_close($socket);
+?>
