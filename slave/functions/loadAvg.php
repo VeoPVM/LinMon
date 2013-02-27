896608b7caf992a996c791708b97aa55716aa1ff
Perri Vardy-Mason <perri@veoserv.com>
2013-02-27 22:39:31 +0000

    Add debug text to loadAvg

--- slave/functions/loadAvg.php
+++ slave/functions/loadAvg.php
@@ -1,8 +1,14 @@
 <?php
 
-function collect_loadAvg() {
+function collect_loadAvg($debug) {
 	$avg = sys_getloadavg();
 	$avg = $avg[0].','.$avg[1].','.$avg[2];
+	
+	if ($debug == TRUE) {
+		$avg = explode(",", $avg);
+		echo "[DEBUG_COLLECT] Load Avg: ".$avg."\n";
+	}
+	
 	return $avg;
 }
 
