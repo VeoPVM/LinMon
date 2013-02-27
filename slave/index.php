e339af1eecd461956c70be73666d41ce2c2d941a
Perri Vardy-Mason <perri@veoserv.com>
2013-02-27 22:39:20 +0000

    Start collecting loadAvg, send debug value

--- slave/index.php
+++ slave/index.php
@@ -11,12 +11,18 @@ include 'functions/checkVersion.php';
 // Debugging Functions
 include 'functions/debug.php';
 
+// Stats Functions
+include 'functions/loadAvg.php';
+
 echo "LinMon slave version ".getVersion("version")." started \n\n";
 echo checkVersion();
 debug_collectionInterval($config['debug'], $config['updateinterval']);
 
 while (true){
 	debug_collectionInfoStart($config['debug']);
+	
+	collect_loadAvg($config['debug']);
+	
     debug_collectionInfoEnd($config['debug']);
 	sleep($config['updateinterval']);
 }
