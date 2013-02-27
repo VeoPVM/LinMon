0bd6c602969e7246580b3dcca8e4369d76a11392
Perri Vardy-Mason <perri@veoserv.com>
2013-02-27 00:01:35 +0000

    Correct line spacing

--- slave/functions/getVersion.php
+++ slave/functions/getVersion.php
@@ -4,15 +4,15 @@ function getVersion($mode) {
 	$version = explode("-", file_get_contents("version"));
 	
 	if (!$version) {
-		echo "Error getting version";
+		echo "Error getting version \n";
 	}
 	
 	if ($mode == "build") {
-		echo $version[0];
+		echo $version[0]." \n";
 	} elseif ($mode == "version") {
-		echo $version[1];
+		echo $version[1]." \n";
 	} else {
-		echo "Invalid version mode";
+		echo "Invalid version mode \n";
 	}
 }
 
