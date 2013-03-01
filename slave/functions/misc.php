d8d0a0b0a54896522a66ef81a26e13b65d0b32e8
Perri <perri@veoserv.com>
2013-03-01 11:49:30 +0000

    Fix uptime and minor re-factoring

--- slave/functions/misc.php
+++ slave/functions/misc.php
@@ -1,38 +1,38 @@
 <?php
 
 function collect_kernel($debug) {
-    $kernel = exec('uname -r')." ".exec('uname -v');
-    
+    $kernel = exec('uname -r') . " " . exec('uname -v');
+
     if ($debug == TRUE) {
-        echo "[DEBUG_COLLECT] Kernel: ".$kernel."\n";
+        echo "[DEBUG_COLLECT] Kernel: " . $kernel . "\n";
     }
-    
+
     return $kernel;
 }
 
 function collect_hostname($debug) {
     $hostname = exec('uname -n');
-    
+
     if ($debug == TRUE) {
-        echo "[DEBUG_COLLECT] Hostname: ".$hostname."\n";
+        echo "[DEBUG_COLLECT] Hostname: " . $hostname . "\n";
     }
-    
+
     return $hostname;
 }
 
 function collect_uptime($debug) {
-    $uptime = exec('uptime');
-    
-    // Replace colons with commas
-    $uptime = str_replace(":", ",", $uptime);
-    
+    $uptime = shell_exec('uptime');
+    $uptime = explode(' up ', $uptime);
+    $uptime = explode(',', $uptime[1]);
+    $uptimehourmin = explode(":", $uptime[1]);
+    $uptime = str_replace("days", "", $uptime[0]);
+    $uptime = $uptime[0].','.$uptimehourmin[0].','.$uptimehourmin[1];
     $uptimearr = explode(",", $uptime);
-    
+
     if ($debug == TRUE) {
-        echo "[DEBUG_COLLECT] Uptime: ".$uptimearr[0]." days ".$uptimearr[1]." hours\n";
+        echo "[DEBUG_COLLECT] Uptime: " . $uptimearr[0] . " days " . $uptimearr[1] . " hours ".$uptimearr[2]." minutes\n";
     }
-    
+
     return $uptime;
 }
-
 ?>
