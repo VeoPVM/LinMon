6a4959b113854bb56d92db9899eb830b07932073
Perri Vardy-Mason <perri@veoserv.com>
2013-03-03 23:38:18 +0000

    Only show update message if current version is less than latest version

--- slave/functions/checkVersion.php
+++ slave/functions/checkVersion.php
@@ -4,7 +4,7 @@ function checkVersion() {
 	$current = explode("-", file_get_contents("version"));
 	$latest = explode("-", file_get_contents("https://raw.github.com/VeoPVM/LinMon/master/slave/version"));
 	
-	if ($current[0] != $latest[0]) {
+	if ($current[0] < $latest[0]) {
 		$current = trim($current[1]);
 		$latest = trim($latest[1]);
 		return "A new version of the slave is available! \nYour version: ".$current." \nLatest version: ".$latest." \n\n";
