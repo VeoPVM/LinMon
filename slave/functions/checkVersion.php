d9acea9bbe44ae354f5135e4b2d018b063458bf6
Perri Vardy-Mason <perri@veoserv.com>
2013-03-15 15:17:51 +0000

    Improve updater

--- slave/functions/checkVersion.php
+++ slave/functions/checkVersion.php
@@ -1,12 +1,17 @@
 <?php
 
 function checkVersion() {
-	$current = explode("-", file_get_contents("version"));
-	$latest = explode("-", file_get_contents("https://raw.github.com/VeoPVM/LinMon/master/slave/version"));
+	$current = file_get_contents("version");
+	$current = str_replace(".", "", $current);
+	// Add 1 to the string so update checking definitely works on versions before 0.1
+	$current = "1".$current;
 	
-	if ($current[0] < $latest[0]) {
-		$current = trim($current[1]);
-		$latest = trim($latest[1]);
+	$latest = file_get_contents("https://raw.github.com/VeoPVM/LinMon/master/slave/version");
+	$latest = str_replace(".", "", $latest);
+	// Add 1 to the string so update checking definitely works on versions before 0.1
+	$latest = "1".$latest;
+	
+	if ($current < $latest) {
 		return "A new version of the slave is available! \nYour version: ".$current." \nLatest version: ".$latest." \n\n";
 	}
 }
--- slave/functions/getVersion.php
+++ slave/functions/getVersion.php
@@ -1,21 +1,13 @@
 <?php
 
-function getVersion($mode) {
-	$version = explode("-", file_get_contents("version"));
+function getVersion() {
+	$version = file_get_contents("version");
 	
 	if (!$version) {
 		return "Error getting version \n";
 	}
 	
-	if ($mode == "build") {
-		$version = trim($version[0]);
-		return $version;
-	} elseif ($mode == "version") {
-		$version = trim($version[1]);
-		return $version;
-	} else {
-		return "Invalid version mode \n";
-	}
+	return $version;
 }
 
 ?>
