fccb54488387b39a7dbea8bb79a5e760822a49a4
Perri <perri@veoserv.com>
2013-04-17 00:24:23 +0100

    Add debug command.  Kill old LinMon processes before running.

--- slave/start.sh
+++ slave/start.sh
@@ -2,6 +2,8 @@
 
 if [ $1 == "start" ] ; then
 	echo -e "\033[01;31mLinMon started.  Press enter to continue\033[00;00m"
+	oldpid=( $(<linmon.pid) )
+	kill -9 $oldpid
 	nohup php index.php &
 	procid=$!
 	echo "$procid" > linmon.pid
@@ -11,4 +13,10 @@ if [ $1 == "stop" ] ; then
 	pid=( $(<linmon.pid) )
 	kill $pid
 	echo "LinMon stopped."
+fi
+
+if [ $1 == "debug" ] ; then
+	oldpid=( $(<linmon.pid) )
+	kill -9 $oldpid
+	php index.php
 fi
