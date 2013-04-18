1edc5364acbdcd4dea97cb89cbc485f9331dc6a2
Perri <perri@veoserv.com>
2013-04-18 14:57:28 +0100

    Trim ifstat output correctly

--- slave/functions/network.php
+++ slave/functions/network.php
@@ -1,15 +1,15 @@
 <?php
 
 function collect_networkUsage($debug, $log) {
-    $usage = exec('ifstat -S 0.1 1');
+    $usage = trim(exec('ifstat -S 0.1 1'));
 
     $usage = explode(" ", $usage);
 
     if ($debug == TRUE) {
-        debug("[DEBUG_COLLECT] Network Usage collected: IN:" . trim($usage[0]) . " | OUT: " . trim($usage[1]) . "\n", $log);
+        debug("[DEBUG_COLLECT] Network Usage collected: IN:" . $usage[0] . " | OUT: " . $usage[1] . "\n", $log);
     }
 
-    $return = trim($usage[0] . ',' . $usage[1]);
+    $return = $usage[0] . ',' . $usage[1];
 
     return $return;
 
