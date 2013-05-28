da1e03784ad4fbf59bd706055eb5d0ea4e2012c2
Perri Vardy-Mason <perri@veoserv.com>
2013-05-28 18:41:01 +0100

    Refactor CPU code.  Add debugging code.

--- slave/functions/cpu.php
+++ slave/functions/cpu.php
@@ -3,13 +3,23 @@
 function collect_cpuUsage($debug, $log) {
     $usage = explode(" ", exec('vmstat 2 2'));
 	
+	$cpu = array();
+	
+	foreach ($usage as $key => $value) {
+		if ($value != "") {
+			array_push($cpu, $value);
+		}
+	}
+	
+	print_r($cpu);
+	
 	// Get CPU usage
-	$cpu = $usage[43];
+	/*$cpu = $usage[43];
 	settype($cpu, "integer");
 	$cpu = 100 - $cpu;
 	
 	// Get WA CPU usage
-	$wa = $usage[44];
+	$wa = $usage[44];*/
 
     if ($debug == TRUE) {
         debug("[DEBUG_COLLECT] CPU Usage: ".$cpu."% WA CPU Usage: ".$wa."%\n", $log);
