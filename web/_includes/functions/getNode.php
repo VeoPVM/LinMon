251efb52f45a42b5792350019d4524aa65b212fe
Your Name <duncan.lewis@me.com>
2013-05-28 17:14:14 +0100

    Fixed Single Node Display Bug
    A systematic error in the code was preventing all nodes from showing.
    Changed system to allow all nodes to be showed now.

--- web/_includes/functions/getNode.php
+++ web/_includes/functions/getNode.php
@@ -1,44 +1,59 @@
 <?php
 function getNode() {
+
+	include('_includes/config/config.php');
 	
 	$connect = db_connect(DBHOST, DBUSER, DBPASS, DBNAME);
 	
-	$getNode = $connect->prepare("SELECT `id`, `loadavg`, `memory` FROM `data` ORDER BY `time` DESC LIMIT 0,1");
+	$getDistinctNodes = $connect->query("SELECT DISTINCT(id) FROM `data`");
 	
-	$getNode->execute();
-	
-	$getNode->bind_result($id, $loadavg, $memory);
-	
-	if ($getNode->error) {
-		try {    
-			throw new Exception("MySQL error $mysqli->error <br> Query:<br> $query", $msqli->errno);    
-		} catch(Exception $e ) {
-			echo "Error No: ".$e->getCode(). " - ". $e->getMessage() . "<br >";
-			echo nl2br($e->getTraceAsString());
-		}
+	$nodes = array();
+	while($node = $getDistinctNodes->fetch_array()){
+	   $nodes[] = $node;
 	}
-
+		
 	
-	while($getNode->fetch()){
-		$memory = explode(",", $memory);
+	for ($i = 0; $i <= ($getDistinctNodes->num_rows - 1); $i++) {	
 		
-		$memory_use = round(100 * ((($memory[0] - $memory[1] + $memory[2] + $memory[3]) - $memory[0]) / $memory[0]));
-		
-		$memory_cache = round(100 * (($memory[2] + $memory[3]) / $memory[0]));
-		
-		
-    	$output = "<tr id=\"".$id."\">";
-        $output .= "<td>".$id."</td>";
-        $output .= "<td class=\"memory\"><div class=\"progress\">";
-        $output .= "<div class=\"bar bar-danger bar-tooltip\" style=\"width: \"0%\" data-toggle=\"tooltip\" title=\"Memory Use\" data-percentage=\"".$memory_use."\">".$memory_use."%</div>";
-		//$output .= "<div class=\"bar bar-success bar-tooltip\" style=\"width: \"0%\" data-toggle=\"tooltip\" title=\"Cache\" data-percentage=\"".$memory_cache."\">".$memory_cache."%</div>";
-        $output .= "</div></td>";
-        $output .= "<td>".$loadavg."</td>";
-        $output .= "<td class=\"status\"><span class=\"label label-success\">Online</span></td>";
-        $output .= "<td class=\"actions\"><div class=\"btn-group\"><a href=\"#\" class=\"btn btn-small  toggle-row\"><i class=\"icon-plus\"></i></a> <a href=\"#\" class=\"btn btn-small \"><i class=\"icon-remove\"></i></a></div></td>";
-        $output .= "</tr>";
-		
-		echo $output;
+	  $getNode = $connect->prepare("SELECT `id`, `loadavg`, `memory` FROM `data` WHERE `id` = '".$nodes[$i][0]."' ORDER BY `time` DESC LIMIT 0,1");
+	  
+	  $getNode->execute();
+	  
+	  $getNode->bind_result($id, $loadavg, $memory);
+	  
+	  if ($getNode->error) {
+		  try {    
+			  throw new Exception("MySQL error $mysqli->error <br> Query:<br> $query", $msqli->errno);    
+		  } catch(Exception $e ) {
+			  echo "Error No: ".$e->getCode(). " - ". $e->getMessage() . "<br >";
+			  echo nl2br($e->getTraceAsString());
+		  }
+	  }
+  
+	  
+	  while($getNode->fetch()){
+		  $memory = explode(",", $memory);
+		  
+		  $memory_use = round(100 * ((($memory[0] - $memory[1] + $memory[2] + $memory[3]) - $memory[0]) / $memory[0]));
+		  
+		 //$memory_cache = round(100 * (($memory[2] + $memory[3]) / $memory[0]));
+		  
+		  
+		  $output = "<tr id=\"".$id."\">";
+		  $output .= "<td>".$id."</td>";
+		  $output .= "<td class=\"memory\"><div class=\"progress\">";
+		  $output .= "<div class=\"bar bar-danger bar-tooltip\" style=\"width: \"0%\" data-toggle=\"tooltip\" title=\"Memory Use\" data-percentage=\"".$memory_use."\">".$memory_use."%</div>";
+		  //$output .= "<div class=\"bar bar-success bar-tooltip\" style=\"width: \"0%\" data-toggle=\"tooltip\" title=\"Cache\" data-percentage=\"".$memory_cache."\">".$memory_cache."%</div>";
+		  $output .= "</div></td>";
+		  $output .= "<td>".$loadavg."</td>";
+			
+		  $output .= "<td class=\"status\"><span class=\"label label-success\">Online</span></td>";
+		  $output .= "<td class=\"actions\"><div class=\"btn-group\"><a href=\"#\" class=\"btn btn-small  toggle-row\"><i class=\"icon-plus\"></i></a> <a href=\"#\" class=\"btn btn-small \"><i class=\"icon-remove\"></i></a></div></td>";
+		  $output .= "</tr>";
+		  
+		  echo $output;
+	  }
+	  
 	}
 }
 
