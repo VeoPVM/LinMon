ed12e910ac4da1df785268a7cd34df889d588bbb
Perri Vardy-Mason <perri@veoserv.com>
2013-05-28 17:15:58 +0100

    Add network debugging

--- slave/functions/network.php
+++ slave/functions/network.php
@@ -4,6 +4,8 @@ function collect_networkUsage($debug, $log) {
     $usage = trim(exec('ifstat -S 0.1 1'));
 
     $usage = explode(" ", $usage);
+	
+	print_r($usage);
 
     if ($debug == TRUE) {
         debug("[DEBUG_COLLECT] Network Usage collected: IN: " . $usage[0] . " KBytes/s | OUT: " . $usage[6] . " KBytes/s\n", $log);
