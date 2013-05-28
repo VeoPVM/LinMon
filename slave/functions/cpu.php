55a0a280dd7db91d62c98d77f1fe7afe53ce7486
Perri Vardy-Mason <perri@veoserv.com>
2013-05-28 16:31:37 +0100

    Add correct debugging text.  Fetch and reeturn CPU and WA CPU

--- slave/functions/cpu.php
+++ slave/functions/cpu.php
@@ -4,12 +4,18 @@ function collect_cpuUsage($debug, $log) {
     $usage = explode(" ", exec('vmstat 1 2'));
 	
 	print_r($usage);
+	
+	// Get CPU usage
+	$cpu = 100 - $usage[43];
+	
+	// Get WA CPU usage
+	$wa = $usage[44];
 
     if ($debug == TRUE) {
-        debug("[DEBUG_COLLECT] ", $log);
+        debug("[DEBUG_COLLECT] CPU Usage: ".$cpu." WA CPU Usage: ".$wa."\n", $log);
     }
-
-    $return = "";
+	
+    $return = $cpu.",".$wa;
 
     return $return;
 
