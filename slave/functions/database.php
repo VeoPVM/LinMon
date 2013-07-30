56bd2ee106e753ac5b586fa5354bd4fc31474bcf
Perri Vardy-Mason <perri@veoserv.com>
2013-07-30 20:37:18 +0100

    Slave will no longer die when database connection is lost

--- slave/functions/database.php
+++ slave/functions/database.php
@@ -3,7 +3,7 @@ function db_connect($host, $user, $pass, $dbname) {
     $connect = new mysqli($host, $user, $pass, $dbname);
 
     if ($connect -> connect_errno) {
-        die("MySQL Error!  Error number: " . $connect -> connect_errno);
+        echo "MySQL Error!  Error number: " . $connect -> connect_errno;
     }
 
     return $connect;
@@ -13,7 +13,7 @@ function db_insert($connect, $id, $loadavg, $memory, $kernel, $hostname, $uptime
     $time = time();
 
     if (!$stmt = $connect -> prepare("INSERT INTO data (time, id, loadavg, memory, kernel, hostname, uptime, users, network, cpu) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")) {
-        die("Can't prepare!");
+        echo "MySQL Error!  Error description: Can't prepare!";
     }
 
     $stmt -> bind_param('isssssssss', $time, $id, $loadavg, $memory, $kernel, $hostname, $uptime, $users, $network, $cpu);
