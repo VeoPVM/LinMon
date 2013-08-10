ef46e5e47380ea336b8be5dfe5a2f87328d57027
Perri Vardy-Mason <perri@veoserv.com>
2013-08-10 18:19:25 +0100

    Fix fatal error crashing slave on database connection failure

--- slave/functions/database.php
+++ slave/functions/database.php
@@ -1,30 +1,32 @@
 <?php
 function db_connect($host, $user, $pass, $dbname) {
-    $connect = new mysqli($host, $user, $pass, $dbname);
+	$connect = new mysqli($host, $user, $pass, $dbname);
 
-    if ($connect -> connect_errno) {
-        echo "MySQL Error!  Error number: " . $connect -> connect_errno;
-    }
+	if ($connect->connect_errno) {
+		echo "MySQL Error!  Error number: " . $connect->connect_errno;
+	}
 
-    return $connect;
+	return $connect;
 }
 
 function db_insert($connect, $id, $loadavg, $memory, $kernel, $hostname, $uptime, $users, $network, $cpu, $version) {
-    $time = time();
+	$time = time();
 
-    if (!$stmt = $connect -> prepare("INSERT INTO data (time, id, loadavg, memory, kernel, hostname, uptime, users, network, cpu, version) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")) {
-        echo "MySQL Error!  Error description: Can't prepare!";
-    }
+	if (!$stmt = $connect->prepare("INSERT INTO data (time, id, loadavg, memory, kernel, hostname, uptime, users, network, cpu, version) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")) {
+		echo "MySQL Error!  Error description: Can't prepare!";
+	} else {
 
-    $stmt -> bind_param('issssssssss', $time, $id, $loadavg, $memory, $kernel, $hostname, $uptime, $users, $network, $cpu, $version);
+		$stmt->bind_param('issssssssss', $time, $id, $loadavg, $memory, $kernel, $hostname, $uptime, $users, $network, $cpu, $version);
 
-    $stmt -> execute();
+		$stmt->execute();
 
-    if ($connect -> error) {
-        echo "[ERROR_DATABASE] " . $connect -> error . "\n";
-    }
+	}
 
-    echo "[INFO_DATABASE] Data submitted to database\n";
+	if ($connect->error) {
+		echo "[ERROR_DATABASE] " . $connect->error . "\n";
+	}
+
+	echo "[INFO_DATABASE] Data submitted to database\n";
 }
 
 function db_close($handle) {
