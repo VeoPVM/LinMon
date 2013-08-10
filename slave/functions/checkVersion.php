3059a4d785b631c26a6e7671f38a855d03d5aefd
Perri Vardy-Mason <perri@veoserv.com>
2013-08-10 10:03:38 +0100

    Use better version comparing code

--- slave/functions/checkVersion.php
+++ slave/functions/checkVersion.php
@@ -2,16 +2,12 @@
 
 function checkVersion() {
     $current = file_get_contents("version");
-    $current = str_replace(".", "", $current);
-    // Add 1 to the string so update checking definitely works on versions before 0.1
-    $current = "1" . $current;
 
     $latest = file_get_contents("https://raw.github.com/VeoPVM/LinMon/master/slave/version");
-    $latest = str_replace(".", "", $latest);
-    // Add 1 to the string so update checking definitely works on versions before 0.1
-    $latest = "1" . $latest;
+	
+	$version = version_compare($current, $latest);
 
-    if ($current < $latest) {
+    if ($version == "-1") {
         return "A new version of the slave is available! \nYour version: " . $current . " \nLatest version: " . $latest . " \n\n";
     }
 }
