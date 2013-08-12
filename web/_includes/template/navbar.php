074b907322c554634e86a2dcc2736a6e0e17e91b
Perri <perri@veoserv.com>
2013-08-12 22:25:14 +0100

    Navbar now links to index.php rather than index.html

--- web/_includes/template/navbar.php
+++ web/_includes/template/navbar.php
@@ -1,6 +1,6 @@
 <div class="navbar navbar-fixed-top">
   <div class="navbar-inner">
-    <div class="container"> <a class="brand" href="index.html">LinMon Dashboard</a>
+    <div class="container"> <a class="brand" href="index.php">LinMon Dashboard</a>
       <div class="nav-collapse">
         
       </div>
