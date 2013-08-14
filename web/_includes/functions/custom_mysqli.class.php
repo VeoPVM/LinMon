be822846d25d783066763521117bbbf58cd569fd
Perri <perri@veoserv.com>
2013-08-14 15:12:29 +0100

    Compare value correctly

--- web/_includes/functions/custom_mysqli.class.php
+++ web/_includes/functions/custom_mysqli.class.php
@@ -4,7 +4,7 @@ class custom_mysqli extends mysqli {
     public function __construct($host, $user, $pass, $db, $compress) {
         parent::init();
 
-        if ($compress = TRUE) {
+        if ($compress == TRUE) {
             parent::real_connect($host, $user, $pass, $db, NULL, NULL, MYSQLI_CLIENT_COMPRESS);
         } else {
             parent::real_connect($host, $user, $pass, $db);
